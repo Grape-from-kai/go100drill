f35e2cdec46432a9cf91f17af513d03992180360
SatoshiOkada <soysoysoup@gmail.com>
2019-12-13 17:27:54 +0900

    一応

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 func main() {
 	if len(os.Args) < 3 {
 		fmt.Println("ERORR: 引数を指定してください")
+		fmt.Println("go run main.go [category] [number]")
+		fmt.Println("e.g.) go run main.go basic 00")
 	} else {
 		dir := os.Args[1]
 		task := os.Args[2]
